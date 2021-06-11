8fc465cc72a924999b2fa34abbd0f00b285c9412
morazow <m.orazow@gmail.com>
2021-06-11 10:07:16 +0200

    Remove unused import.

--- src/test/scala/com/exasol/cloudetl/kafka/KafkaConsumerPropertiesTest.scala
+++ src/test/scala/com/exasol/cloudetl/kafka/KafkaConsumerPropertiesTest.scala
@@ -2,7 +2,6 @@ package com.exasol.cloudetl.kafka
 
 import java.nio.file.Path
 import java.nio.file.Paths
-import java.util.Locale.ENGLISH
 
 import com.exasol.{ExaConnectionInformation, ExaMetadata}
 
