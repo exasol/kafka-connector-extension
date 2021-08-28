71b4fb27836cfac0ff47ae6dc8aef2c3d02df181
morazow <m.orazow@gmail.com>
2021-08-29 00:36:00 +0200

    Apply formatting.

--- src/main/scala/com/exasol/cloudetl/kafka/deserialization/FieldConverter.scala
+++ src/main/scala/com/exasol/cloudetl/kafka/deserialization/FieldConverter.scala
@@ -6,18 +6,16 @@ class FieldConverter(
   outputColumnTypes: Seq[Class[_]]
 ) {
 
-  final def convertRow(row: Seq[Any]): Seq[Any] = {
-    row.zipWithIndex map (x => convert(outputColumnTypes(x._2), x._1))
-  }
+  final def convertRow(row: Seq[Any]): Seq[Any] =
+    row.zipWithIndex.map(x => convert(outputColumnTypes(x._2), x._1))
 
-  final def convert(column_type: Class[_], value: Any): Any = {
+  final def convert(column_type: Class[_], value: Any): Any =
     if (column_type == classOf[Timestamp]) {
       value match {
         case x: Long => new Timestamp(x)
-        case _ => value
+        case _       => value
       }
     } else {
       value
     }
-  }
 }
