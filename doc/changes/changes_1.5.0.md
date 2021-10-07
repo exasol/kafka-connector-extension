234959c4b90ca11d1bbeca0d26484716d9c448d6
morazow <m.orazow@gmail.com>
2021-10-07 11:21:55 +0200

    Added changelog.

---
+++ doc/changes/changes_1.5.0.md
@@ -0,0 +1,24 @@
+# Kafka Connector Extension 1.5.0, released 2021-10-??
+
+Code name:
+
+## Summary
+
+## Refactoring
+
+* #55: Added optionally check for keystore or truststore files when using `SASL_SSL` protocol
+
+## Dependency Updates
+
+### Runtime Dependency Updates
+
+### Test Dependency Updates
+
+* Updated `org.scalatest:scalatest:3.2.9` to `3.2.10`
+* Updated `com.exasol:exasol-testcontainers:4.0.1` to `5.1.0`
+* Updated `com.exasol:hamcrest-resultset-matcher:1.4.1` to `1.5.0`
+* Updated `com.sksamuel.avro4s:avro4s-core:4.0.10` to `4.0.11`
+
+### Plugin Updates
+
+* Updated `org.scoverage:sbt-scoverage:1.8.2` to `1.9.0`
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,16 +13,15 @@ object Dependencies {
   private val ScalaCollectionCompatVersion = "2.5.0"
 
   // Test dependencies versions
-  private val ScalaTestVersion = "3.2.9"
+  private val ScalaTestVersion = "3.2.10"
   private val ScalaTestPlusVersion = "1.0.0-M2"
   private val MockitoCoreVersion = "3.12.4"
   private val KafkaSchemaRegistryVersion = "6.2.0"
   private val ExasolTestDBBuilderVersion = "3.2.1"
-  private val ExasolTestContainersVersion = "4.0.1"
-  private val ExasolHamcrestMatcherVersion = "1.4.1"
+  private val ExasolTestContainersVersion = "5.1.0"
+  private val ExasolHamcrestMatcherVersion = "1.5.0"
 
   val Resolvers: Seq[Resolver] = Seq(
-     Resolver.mavenLocal,
     "jitpack.io" at "https://jitpack.io",
     "Confluent Maven Repo" at "https://packages.confluent.io/maven/",
     "Exasol Releases" at "https://maven.exasol.com/artifactory/exasol-releases"
@@ -55,7 +54,7 @@ object Dependencies {
     "com.exasol" % "test-db-builder-java" % ExasolTestDBBuilderVersion,
     "com.exasol" % "hamcrest-resultset-matcher" % ExasolHamcrestMatcherVersion,
     "io.confluent" % "kafka-streams-avro-serde" % KafkaAvroSerializerVersion,
-    "com.sksamuel.avro4s" %% "avro4s-core" % "4.0.10"
+    "com.sksamuel.avro4s" %% "avro4s-core" % "4.0.11"
   ).map(_ % Test)
 
   lazy val AllDependencies: Seq[ModuleID] = RuntimeDependencies ++ TestDependencies
