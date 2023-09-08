578a080099e0c28b68401f974e75c09f91ce9038
Pieterjan Spoelders <pj.spoelders@gmail.com>
2023-09-08 14:05:54 +0200

    start kafka testcontainer explicitly

--- src/test/java/com/exasol/cloudetl/kafka/KafkaTestSetup.java
+++ src/test/java/com/exasol/cloudetl/kafka/KafkaTestSetup.java
@@ -5,19 +5,10 @@ import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 import java.util.logging.Logger;
 
-
-import org.apache.kafka.clients.admin.Admin;
-import org.apache.kafka.clients.admin.AdminClientConfig;
-import org.apache.kafka.clients.admin.CreateTopicsResult;
-import org.apache.kafka.clients.admin.NewTopic;
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.admin.*;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.KafkaFuture;
-import org.testcontainers.containers.GenericContainer;
-import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.utility.DockerImageName;
-
 
 class KafkaTestSetup implements AutoCloseable {
 
@@ -29,43 +20,42 @@ class KafkaTestSetup implements AutoCloseable {
     private KafkaTestSetup(final org.testcontainers.containers.KafkaContainer container, final String topicName) {
         this.container = container;
         this.topicName = topicName;
-        //this.client = client;
+        // this.client = client;
     }
 
     public org.testcontainers.containers.KafkaContainer getContainer() {
         return this.container;
     }
+
     public String getTopicName() {
         return this.topicName;
     }
+
     static KafkaTestSetup create() throws ExecutionException, InterruptedException {
         @SuppressWarnings("resource") // Container will be stopped in close() method
-                DockerImageName dockerImageName = DockerImageName.parse("confluentinc/cp-kafka:6.2.1");
-        final org.testcontainers.containers.KafkaContainer container = new org.testcontainers.containers.KafkaContainer(dockerImageName);
-        var bootstrapServers = container.getBootstrapServers();
-        //return String.format("PLAINTEXT://%s:%s", this.getHost(), this.getMappedPort(9093));
-        //container.start();
+        final DockerImageName dockerImageName = DockerImageName.parse("confluentinc/cp-kafka:6.2.1");
+        final org.testcontainers.containers.KafkaContainer container = new org.testcontainers.containers.KafkaContainer(
+                dockerImageName);
+        container.start();
+        // return String.format("PLAINTEXT://%s:%s", this.getHost(), this.getMappedPort(9093));
+        // container.start();
 
-        Properties properties = new Properties();
-        properties.put(
-                AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, container.getBootstrapServers()
-        );
-        String topicName = "testTopic";
-        //CREATE TOPIC AND WAIT
+        final Properties properties = new Properties();
+        properties.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, container.getBootstrapServers());
+        final String topicName = "testTopic";
+        // CREATE TOPIC AND WAIT
         try (Admin admin = Admin.create(properties)) {
-            int partitions = 1;
-            short replicationFactor = 1;
-            NewTopic newTopic = new NewTopic(topicName, partitions, replicationFactor);
+            final int partitions = 1;
+            final short replicationFactor = 1;
+            final NewTopic newTopic = new NewTopic(topicName, partitions, replicationFactor);
 
-            CreateTopicsResult result = admin.createTopics(
-                    Collections.singleton(newTopic)
-            );
+            final CreateTopicsResult result = admin.createTopics(Collections.singleton(newTopic));
 
-            KafkaFuture<Void> future = result.values().get(topicName);
-            var createTopicsResult = future.get();
+            final KafkaFuture<Void> future = result.values().get(topicName);
+            final var createTopicsResult = future.get();
         }
-        //PRODUCE
-        Properties producerProps = new Properties();
+        // PRODUCE
+        final Properties producerProps = new Properties();
         producerProps.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, container.getBootstrapServers());
         producerProps.put("acks", "all");
         producerProps.put("retries", 0);
@@ -75,22 +65,19 @@ class KafkaTestSetup implements AutoCloseable {
         producerProps.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
         producerProps.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 
-        Producer<String, String> producer = new KafkaProducer<>(producerProps);
-        //for (int i = 0; i < 100; i++)
-            producer.send(new ProducerRecord<String, String>(topicName, Integer.toString(1), "OK"));
-            producer.send(new ProducerRecord<String, String>(topicName, Integer.toString(2), "WARN"));
+        final Producer<String, String> producer = new KafkaProducer<>(producerProps);
+        // for (int i = 0; i < 100; i++)
+        producer.send(new ProducerRecord<String, String>(topicName, Integer.toString(1), "OK"));
+        producer.send(new ProducerRecord<String, String>(topicName, Integer.toString(2), "WARN"));
         producer.close();
-        //RETURN kafkaTestSetup object
-        return new KafkaTestSetup(container,topicName);
+        // RETURN kafkaTestSetup object
+        return new KafkaTestSetup(container, topicName);
     }
 
     @Override
     public void close() {
-        //this.client.shutdown();
+        // this.client.shutdown();
         this.container.close();
     }
-
-
-
 
 }
