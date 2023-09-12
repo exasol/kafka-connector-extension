063906c36f0979f4646adb07620c93d44cd16e87
Pieterjan Spoelders <pj.spoelders@gmail.com>
2023-09-12 10:27:30 +0200

    change import into statement

--- src/test/java/com/exasol/cloudetl/kafka/ExtensionIT.java
+++ src/test/java/com/exasol/cloudetl/kafka/ExtensionIT.java
@@ -52,7 +52,8 @@ class ExtensionIT {
     private static ExasolObjectFactory exasolObjectFactory;
 
     @BeforeAll
-    static void setup() throws FileNotFoundException, BucketAccessException, TimeoutException, SQLException, ExecutionException, InterruptedException {
+    static void setup() throws FileNotFoundException, BucketAccessException, TimeoutException, SQLException,
+            ExecutionException, InterruptedException {
         exasolTestSetup = new ExasolTestSetupFactory(Paths.get("no-cloud-setup")).getTestSetup();
         setup = ExtensionManagerSetup.create(exasolTestSetup, ExtensionBuilder.createDefaultNpmBuilder(
                 EXTENSION_SOURCE_DIR, EXTENSION_SOURCE_DIR.resolve("dist").resolve(EXTENSION_ID)));
@@ -237,49 +238,53 @@ class ExtensionIT {
                 .project("kafka-connector-extension") //
                 .build();
     }
-//TODo
+
+    // TODo
     private void verifyImportWorks() {
         final ExasolSchema schema = exasolObjectFactory.createSchema("TESTING_SCHEMA_" + System.currentTimeMillis());
-        //final String connectionName = "KAFKA_CONNECTION";
+        // final String connectionName = "KAFKA_CONNECTION";
 
-        try {//(final KafkaStream stream = kafkaSetup.createStream("extension-stream", 1)) {
+        try {// (final KafkaStream stream = kafkaSetup.createStream("extension-stream", 1)) {
 
-            //stream.putRecord(sensorDataPayload(1, "OK"), PARTITION_KEY);
-            //stream.putRecord(sensorDataPayload(2, "WARN"), PARTITION_KEY);
+            // stream.putRecord(sensorDataPayload(1, "OK"), PARTITION_KEY);
+            // stream.putRecord(sensorDataPayload(2, "WARN"), PARTITION_KEY);
 
             final Table targetTable = schema.createTableBuilder("TARGET")
-                    //TODO: check variables
-                    .column("SENSOR_ID", "INTEGER")
-                    .column("STATUS", "VARCHAR(10)")
+                    // TODO: check variables
+                    .column("SENSOR_ID", "INTEGER").column("STATUS", "VARCHAR(10)")
                     //
-                    .column("KAFKA_PARTITION", "DECIMAL(18, 0)")
-                    .column("KAFKA_OFFSET", "DECIMAL(36, 0)").build();
-            //CREATE CONNECTION (see https://github.com/exasol/kafka-connector-extension/blob/main/doc/user_guide/user_guide.md#importing-records)
-            //OPTIONAL
+                    .column("KAFKA_PARTITION", "DECIMAL(18, 0)").column("KAFKA_OFFSET", "DECIMAL(36, 0)").build();
+            // CREATE CONNECTION (see
+            // https://github.com/exasol/kafka-connector-extension/blob/main/doc/user_guide/user_guide.md#importing-records)
+            // OPTIONAL
             executeKafkaImport(targetTable, kafkaSetup);
 
             assertQueryResult(
                     "select sensor_id, status, kafka_partition,kafka_offset from " + targetTable.getFullyQualifiedName()
                             + " order by sensor_id",
-                    table("BIGINT", "VARCHAR", "DECIMAL","DECIMAL") //
-                            .row(1L, "OK", 1.0,1.0) //
-                            .row(2L, "WARN", 1.0,1.0) //
+                    table("BIGINT", "VARCHAR", "DECIMAL", "DECIMAL") //
+                            .row(1L, "OK", 1.0, 1.0) //
+                            .row(2L, "WARN", 1.0, 1.0) //
                             .matches());
         } finally {
             schema.drop();
         }
     }
-//TODO
+
+    // TODO
     //
-    private void executeKafkaImport(final Table targetTable,
-            final KafkaTestSetup kafkaSetup) {
-        String bootstrapServers = kafkaSetup.getContainer().getBootstrapServers();
+    private void executeKafkaImport(final Table targetTable, final KafkaTestSetup kafkaSetup) {
+        final String bootstrapServers = kafkaSetup.getContainer().getBootstrapServers();
 
         executeStatement("OPEN SCHEMA " + ExtensionManagerSetup.EXTENSION_SCHEMA_NAME);
         final String sql = "IMPORT INTO " + targetTable.getFullyQualifiedName() + "\n" + //
                 " FROM SCRIPT " + ExtensionManagerSetup.EXTENSION_SCHEMA_NAME + ".KAFKA_CONSUMER WITH\n" + //
-                " BOOTSTRAP_SERVERS = '" + bootstrapServers+ "'\n" + //
-                //" SCHEMA_REGISTRY_URL = '" + kafkaConnection + "'\n" + //is an URL to the Schema Registry server. This is only required if you are importing Avro records. It is used to retrieve Avro schemas of Kafka topics. Avro is set as default record value format.
+                " BOOTSTRAP_SERVERS = '" + bootstrapServers + "'\n" + //
+                " RECORD_KEY_FORMAT='string'" + "'\n" + //
+                " RECORD_VALUE_FORMAT='string'" + "'\n" + //
+                // " SCHEMA_REGISTRY_URL = '" + kafkaConnection + "'\n" + //is an URL to the Schema Registry server.
+                // This is only required if you are importing Avro records. It is used to retrieve Avro schemas of Kafka
+                // topics. Avro is set as default record value format.
                 " TOPIC_NAME = '" + kafkaSetup.getTopicName() + "'\n" + //
                 " TABLE_NAME = '" + targetTable.getFullyQualifiedName() + "'\n" + //
                 " GROUP_ID = 'exasol-kafka-udf-consumers'\n";
