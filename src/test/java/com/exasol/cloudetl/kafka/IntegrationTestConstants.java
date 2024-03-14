7e53542fa25da11e07ee2a8cca8ccc0945f4087c
Christoph Pirkl <christoph.pirkl@exasol.com>
2024-03-14 16:08:48 +0100

    Update default Exasol version

--- src/test/java/com/exasol/cloudetl/kafka/IntegrationTestConstants.java
+++ src/test/java/com/exasol/cloudetl/kafka/IntegrationTestConstants.java
@@ -6,7 +6,7 @@ public class IntegrationTestConstants {
     public static final String PROJECT_VERSION = MavenProjectVersionGetter.getCurrentProjectVersion();
     public static String JAR_FILE_NAME = "exasol-kafka-connector-extension-" + PROJECT_VERSION + ".jar";
     public static String TEST_SCHEMA_NAME = "kafka_schema";
-    public static String DEFAULT_EXASOL_DOCKER_IMAGE = "7.1.24";
+    public static String DEFAULT_EXASOL_DOCKER_IMAGE = "8.24.0";
     public static String LOCALSTACK_DOCKER_IMAGE = "localstack/localstack:2.2";
     public static String DOCKER_IP_ADDRESS = "172.17.0.1";
 }
