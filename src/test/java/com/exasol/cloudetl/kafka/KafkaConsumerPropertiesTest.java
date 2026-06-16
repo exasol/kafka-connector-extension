bb1f11fdbf25ab7a2cf369153550b10ee8f7d96f
Christoph Pirkl <christoph.pirkl@exasol.com>
2026-06-16 12:51:05 +0200

    Restore original Scala assertions

--- src/test/java/com/exasol/cloudetl/kafka/KafkaConsumerPropertiesTest.java
+++ src/test/java/com/exasol/cloudetl/kafka/KafkaConsumerPropertiesTest.java
@@ -46,69 +46,144 @@ class KafkaConsumerPropertiesTest {
     }
 
     @Test
-    void requiredAndDefaultBaseProperties() {
+    void getBootstrapServersReturnsBootstrapServersPropertyValue() {
         assertGetter("BOOTSTRAP_SERVERS", "kafka01.example.com,kafka02.example.com",
                 () -> base().getBootstrapServers());
+    }
+
+    @Test
+    void getBootstrapServersThrowsIfBootstrapServersPropertyIsNotSet() {
         expectMissingProperty("BOOTSTRAP_SERVERS", () -> base().getBootstrapServers());
+    }
+
+    @Test
+    void getGroupIdReturnsUserProvidedValue() {
         assertGetter("GROUP_ID", "groupId", () -> base().getGroupId());
-        this.properties.clear();
+    }
+
+    @Test
+    void getGroupIdReturnsDefaultValueIfGroupIdIsNotSet() {
         assertEquals("EXASOL_KAFKA_UDFS_CONSUMERS", base().getGroupId());
+    }
+
+    @Test
+    void getTopicsReturnsTopicsPropertyValue() {
         assertGetter("TOPIC_NAME", "Metamorphosis", () -> base().getTopic());
+    }
+
+    @Test
+    void getTopicsThrowsIfTopicsPropertyIsNotSet() {
         expectMissingProperty("TOPIC_NAME", () -> base().getTopic());
+    }
+
+    @Test
+    void getTableNameReturnsExasolTableNamePropertyValue() {
         assertGetter("TABLE_NAME", "table", () -> base().getTableName());
+    }
+
+    @Test
+    void getTableNameThrowsIfTableNamePropertyIsNotSet() {
         expectMissingProperty("TABLE_NAME", () -> base().getTableName());
     }
 
     @Test
-    void numericProperties() {
+    void getPollTimeoutMsReturnsProvidedPollTimeoutValue() {
         assertGetter("POLL_TIMEOUT_MS", "10", () -> base().getPollTimeoutMs(), 10L);
-        this.properties.clear();
+    }
+
+    @Test
+    void getPollTimeoutMsReturnsDefaultValueIfPollTimeoutIsNotSet() {
         assertEquals(30000L, base().getPollTimeoutMs());
+    }
+
+    @Test
+    void getPollTimeoutMsThrowsIfValueCannotBeConvertedToLong() {
         this.properties.put("POLL_TIMEOUT_MS", "1l");
         assertThrows(NumberFormatException.class, () -> base().getPollTimeoutMs());
+    }
 
-        this.properties.clear();
+    @Test
+    void getMinRecordsPerRunReturnsProvidedMinimumValue() {
         assertGetter("MIN_RECORDS_PER_RUN", "7", () -> base().getMinRecordsPerRun(), 7);
-        this.properties.clear();
+    }
+
+    @Test
+    void getMinRecordsPerRunReturnsDefaultValueIfMinimumValueIsNotSet() {
         assertEquals(100, base().getMinRecordsPerRun());
+    }
+
+    @Test
+    void getMinRecordsPerRunThrowsIfValueCannotBeConvertedToInt() {
         this.properties.put("MIN_RECORDS_PER_RUN", "e");
         assertThrows(NumberFormatException.class, () -> base().getMinRecordsPerRun());
+    }
 
-        this.properties.clear();
+    @Test
+    void getMaxRecordsPerRunReturnsProvidedMaximumValue() {
         assertGetter("MAX_RECORDS_PER_RUN", "43", () -> base().getMaxRecordsPerRun(), 43);
-        this.properties.clear();
+    }
+
+    @Test
+    void getMaxRecordsPerRunReturnsDefaultValueIfMaximumValueIsNotSet() {
         assertEquals(1000000, base().getMaxRecordsPerRun());
+    }
+
+    @Test
+    void getMaxRecordsPerRunThrowsIfValueCannotBeConvertedToInt() {
         this.properties.put("MAX_RECORDS_PER_RUN", "max");
         assertThrows(NumberFormatException.class, () -> base().getMaxRecordsPerRun());
     }
 
     @Test
-    void securityProtocolFlags() {
+    void sslAndSaslAreDisabledIfSecurityProtocolIsNotSet() {
         assertFalse(base().isSSLEnabled());
         assertFalse(base().isSASLEnabled());
+    }
 
+    @Test
+    void sslIsEnabledAndSaslIsDisabledIfSecurityProtocolIsSsl() {
         this.properties.put("SECURITY_PROTOCOL", "SSL");
         assertTrue(base().isSSLEnabled());
         assertFalse(base().isSASLEnabled());
+    }
 
+    @Test
+    void sslIsDisabledAndSaslIsEnabledIfSecurityProtocolIsSaslPlaintext() {
         this.properties.put("SECURITY_PROTOCOL", "SASL_PLAINTEXT");
         assertFalse(base().isSSLEnabled());
         assertTrue(base().isSASLEnabled());
+    }
 
+    @Test
+    void sslIsDisabledAndSaslIsEnabledIfSecurityProtocolIsSaslSsl() {
         this.properties.put("SECURITY_PROTOCOL", "SASL_SSL");
         assertFalse(base().isSSLEnabled());
         assertTrue(base().isSASLEnabled());
     }
 
     @Test
-    void schemaRegistryProperties() {
-        assertFalse(base().hasSchemaRegistryUrl());
+    void hasSchemaRegistryUrlReturnsTrueIfSchemaRegistryUrlIsProvided() {
         assertGetter("SCHEMA_REGISTRY_URL", "http://a-schema.url", () -> base().getSchemaRegistryUrl());
         assertTrue(base().hasSchemaRegistryUrl());
+    }
 
-        this.properties.clear();
+    @Test
+    void hasSchemaRegistryUrlReturnsFalseIfSchemaRegistryUrlIsNotSet() {
+        assertFalse(base().hasSchemaRegistryUrl());
+    }
+
+    @Test
+    void getSchemaRegistryUrlReturnsSchemaRegistryUrlPropertyValue() {
+        assertGetter("SCHEMA_REGISTRY_URL", "http://a-schema.url", () -> base().getSchemaRegistryUrl());
+    }
+
+    @Test
+    void getSchemaRegistryUrlThrowsIfSchemaRegistryUrlPropertyIsNotSet() {
         expectMissingProperty("SCHEMA_REGISTRY_URL", () -> base().getSchemaRegistryUrl());
+    }
 
+    @Test
+    void getPropertiesThrowsIfSchemaRegistryIsNotSetAndRecordFormatIsAvro() {
         final var avroProperties = map(entry("BOOTSTRAP_SERVERS", "server"), entry("RECORD_FORMAT", "avro"));
         final IllegalArgumentException thrown = assertThrows(IllegalArgumentException.class,
                 () -> KafkaConsumerPropertiesSupport.create(avroProperties).getProperties());
@@ -116,77 +191,195 @@ class KafkaConsumerPropertiesTest {
     }
 
     @Test
-    void kafkaConsumerDefaultsAndOverrides() {
+    void getMaxPollRecordsReturnsMaxPollRecordsValue() {
         assertGetter("MAX_POLL_RECORDS", "9", () -> base().getMaxPollRecords());
+    }
+
+    @Test
+    void getMaxPollRecordsReturnsDefaultValueIfMaxPollRecordsIsNotSet() {
         assertDefault("500", () -> base().getMaxPollRecords());
+    }
+
+    @Test
+    void getFetchMinBytesReturnsMinimumFetchBytesPropertyValue() {
         assertGetter("FETCH_MIN_BYTES", "23", () -> base().getFetchMinBytes());
+    }
+
+    @Test
+    void getFetchMinBytesReturnsDefaultValueIfPropertyIsNotSet() {
         assertDefault("1", () -> base().getFetchMinBytes());
+    }
+
+    @Test
+    void getFetchMaxBytesReturnsMaximumFetchBytesPropertyValue() {
         assertGetter("FETCH_MAX_BYTES", "27", () -> base().getFetchMaxBytes());
+    }
+
+    @Test
+    void getFetchMaxBytesReturnsDefaultValueIfPropertyIsNotSet() {
         assertDefault("52428800", () -> base().getFetchMaxBytes());
+    }
+
+    @Test
+    void getMaxPartitionFetchBytesReturnsMaximumPartitionFetchBytesPropertyValue() {
         assertGetter("MAX_PARTITION_FETCH_BYTES", "4", () -> base().getMaxPartitionFetchBytes());
+    }
+
+    @Test
+    void getMaxPartitionFetchBytesReturnsDefaultValueIfPropertyIsNotSet() {
         assertDefault("1048576", () -> base().getMaxPartitionFetchBytes());
+    }
+
+    @Test
+    void getOffsetResetStrategyReturnsDefaultValueEarliestIfPropertyIsNotSet() {
         assertDefault("earliest", () -> base().getAutoOffsetReset());
+    }
+
+    @Test
+    void getSecurityProtocolReturnsUserProvidedSecurityProtocolPropertyValue() {
         assertGetter("SECURITY_PROTOCOL", "SSL", () -> base().getSecurityProtocol());
+    }
+
+    @Test
+    void getSecurityProtocolReturnsDefaultValueIfSecurityProtocolIsNotSet() {
         assertDefault("PLAINTEXT", () -> base().getSecurityProtocol());
     }
 
     @Test
-    void sslProperties() {
+    void getSslKeyPasswordReturnsSslKeyPasswordPropertyValue() {
         assertGetter("SSL_KEY_PASSWORD", "1337", () -> base().getSSLKeyPassword());
+    }
+
+    @Test
+    void getSslKeyPasswordThrowsIfSslKeyPasswordPropertyIsNotSet() {
         expectMissingProperty("SSL_KEY_PASSWORD", () -> base().getSSLKeyPassword());
+    }
+
+    @Test
+    void getSslKeystorePasswordReturnsSslKeystorePasswordPropertyValue() {
         assertGetter("SSL_KEYSTORE_PASSWORD", "p@ss", () -> base().getSSLKeystorePassword());
+    }
+
+    @Test
+    void getSslKeystorePasswordThrowsIfSslKeystorePasswordPropertyIsNotSet() {
         expectMissingProperty("SSL_KEYSTORE_PASSWORD", () -> base().getSSLKeystorePassword());
+    }
+
+    @Test
+    void getSslKeystoreLocationReturnsSslKeystoreLocationPropertyValue() {
         assertGetter("SSL_KEYSTORE_LOCATION", "/path/keystore.jks", () -> base().getSSLKeystoreLocation());
+    }
+
+    @Test
+    void getSslKeystoreLocationThrowsIfSslKeystoreLocationPropertyIsNotSet() {
         expectMissingProperty("SSL_KEYSTORE_LOCATION", () -> base().getSSLKeystoreLocation());
+    }
+
+    @Test
+    void getSslTruststorePasswordReturnsSslTruststorePasswordPropertyValue() {
         assertGetter("SSL_TRUSTSTORE_PASSWORD", "tp@ss", () -> base().getSSLTruststorePassword());
+    }
+
+    @Test
+    void getSslTruststorePasswordThrowsIfSslTruststorePasswordPropertyIsNotSet() {
         expectMissingProperty("SSL_TRUSTSTORE_PASSWORD", () -> base().getSSLTruststorePassword());
+    }
+
+    @Test
+    void getSslTruststoreLocationReturnsSslTruststoreLocationPropertyValue() {
         assertGetter("SSL_TRUSTSTORE_LOCATION", "/path/truststore.jks", () -> base().getSSLTruststoreLocation());
+    }
+
+    @Test
+    void getSslTruststoreLocationThrowsIfSslTruststoreLocationPropertyIsNotSet() {
         expectMissingProperty("SSL_TRUSTSTORE_LOCATION", () -> base().getSSLTruststoreLocation());
+    }
+
+    @Test
+    void getSslEndpointIdentificationAlgorithmReturnsUserProvidedPropertyValue() {
         assertGetter("SSL_ENDPOINT_IDENTIFICATION_ALGORITHM", "none", () -> base().getSSLEndpointIdentificationAlgorithm());
+    }
+
+    @Test
+    void getSslEndpointIdentificationAlgorithmReturnsDefaultValueIfItIsNotSet() {
         assertDefault("https", () -> base().getSSLEndpointIdentificationAlgorithm());
     }
 
     @Test
-    void saslJaasConfigCanComeFromCredentialsOrFile() {
+    void getSaslJaasConfigReturnsJaasContentWithUsernameAndPassword() {
         this.properties = mutableMap("SASL_MECHANISM", "PLAIN", "SASL_USERNAME", "kafka", "SASL_PASSWORD", "kafkapw");
         assertEquals("org.apache.kafka.common.security.plain.PlainLoginModule required "
                 + "username=\"kafka\" password=\"kafkapw\";", base().getSASLJaasConfig());
+    }
 
+    @Test
+    void getSaslJaasConfigReturnsJaasContentFromFile() {
         this.properties = mutableMap("SASL_JAAS_LOCATION", this.dummySaslJaasFile.toString());
         assertEquals("org.apache.kafka.common.security.oauthbearer.OAuthBearerLoginModule required "
                 + "unsecuredLoginStringClaim_sub=\"alice\";", base().getSASLJaasConfig());
     }
 
     @Test
-    void consumeAllOffsetsAndRecordFormats() {
+    void isConsumeAllOffsetsEnabledReturnsTrueIfItIsSetToTrue() {
         this.properties.put("CONSUME_ALL_OFFSETS", "true");
         assertTrue(base().isConsumeAllOffsetsEnabled());
-        this.properties.clear();
+    }
+
+    @Test
+    void isConsumeAllOffsetsEnabledReturnsFalseIfItIsNotSet() {
         assertFalse(base().isConsumeAllOffsetsEnabled());
+    }
 
+    @Test
+    void getRecordKeyFormatReturnsUserProvidedPropertyValue() {
         assertGetter("RECORD_KEY_FORMAT", "keyFormat", () -> base().getRecordKeyFormat(), "keyformat");
-        assertDefault("string", () -> base().getRecordKeyFormat());
+    }
 
+    @Test
+    void getRecordKeyFormatReturnsDefaultValueIfNotSet() {
+        assertDefault("string", () -> base().getRecordKeyFormat());
+    }
+
+    @Test
+    void getRecordValueFormatReturnsUserProvidedPropertyValue() {
         final Map<String, String> values = Map.of("avro", "avro", "JsOn", "json", "String", "string");
         for (final Map.Entry<String, String> entry : values.entrySet()) {
             this.properties = mutableMap("RECORD_VALUE_FORMAT", entry.getKey());
             assertEquals(entry.getValue(), base().getRecordValueFormat());
         }
+    }
+
+    @Test
+    void getRecordValueFormatUsesRecordFormatIfItIsSet() {
         this.properties = mutableMap("RECORD_FORMAT", "new_FormaT");
         assertEquals("new_format", base().getRecordValueFormat());
-        this.properties.clear();
+    }
+
+    @Test
+    void getRecordValueFormatReturnsDefaultValueIfNotSet() {
         assertEquals("avro", base().getRecordValueFormat());
     }
 
     @Test
-    void recordFields() {
+    void getRecordFieldsReturnsUserProvidedPropertyValue() {
         this.properties = mutableMap("RECORD_FIELDS", "value.name,value.Sur_Name, value.address1, timestamp");
         assertSeqEquals(Arrays.asList("value.name", "value.Sur_Name", "value.address1", "timestamp"),
                 base().getRecordFields());
-        this.properties.clear();
+    }
+
+    @Test
+    void getRecordFieldsReturnsDefaultValueIfNotSet() {
         assertSeqEquals(List.of("value.*"), base().getRecordFields());
+    }
+
+    @Test
+    void getRecordFieldsReturnsDefaultValueWhenRecordValueIsNotAvro() {
         this.properties = mutableMap("RECORD_VALUE_FORMAT", "json");
         assertSeqEquals(List.of("value"), base().getRecordFields());
+    }
+
+    @Test
+    void getRecordFieldsReturnsDefaultValueWhenSingleJsonColumnIsRequested() {
         this.properties = mutableMap("AS_JSON_DOC", "true");
         assertSeqEquals(List.of("value"), base().getRecordFields());
     }
@@ -268,44 +461,84 @@ class KafkaConsumerPropertiesTest {
     }
 
     @Test
-    void sslSecurityValidation() throws Exception {
+    void applyReturnsSslEnabledConsumerProperties() throws Exception {
         final var props = getSecurityEnabledConsumerProperties("SSL", this.dummyKeystoreFile, this.dummyTruststoreFile, null);
         assertEquals(this.dummyKeystoreFile.toString(), props.getSSLKeystoreLocation());
         assertEquals(this.dummyTruststoreFile.toString(), props.getSSLTruststoreLocation());
+    }
 
+    @Test
+    void applyThrowsIfSslKeystoreJksFileIsNotAvailable() {
         assertMissingSecurityFile("SSL", Paths.get("ssl_keystore_file"), this.dummyTruststoreFile, null,
                 "Unable to find the SSL keystore file");
+    }
+
+    @Test
+    void applyThrowsIfSslTruststoreJksFileIsNotAvailable() {
         assertMissingSecurityFile("SSL", this.dummyKeystoreFile, Paths.get("ssl_truststore_file"), null,
                 "Unable to find the SSL truststore file");
     }
 
     @Test
-    void saslSslSecurityValidationAndProperties() throws Exception {
+    void applyReturnsSslSaslEnabledConsumerProperties() throws Exception {
         assertEquals("PLAIN", getSecurityEnabledConsumerProperties("SASL_SSL", null, null, null).getSASLMechanism());
+    }
 
+    @Test
+    void applyOptionallyChecksKeystoreAndTruststoreFilesWithSslSaslProtocol() throws Exception {
         final var props = getSecurityEnabledConsumerProperties("SASL_SSL", this.dummyKeystoreFile,
                 this.dummyTruststoreFile, null);
         assertTrue(props.getSASLJaasConfig().contains("\"pass\""));
         assertEquals(this.dummyKeystoreFile.toString(), props.getSSLKeystoreLocation());
         assertEquals(this.dummyTruststoreFile.toString(), props.getSSLTruststoreLocation());
-
-        assertMissingSecurityFile("SASL_SSL", Paths.get("ssl_keystore_file"), null, null,
-                "Unable to find the SSL keystore file");
-        assertMissingSecurityFile("SASL_SSL", this.dummyKeystoreFile, Paths.get("ssl_truststore_file"), null,
-                "Unable to find the SSL truststore file");
-
-        assertEquals(this.dummyKeystoreFile.toString(),
-                getSecurityEnabledConsumerProperties("SASL_SSL", this.dummyKeystoreFile, null, null).getProperties()
-                        .get(SSL_KEYSTORE_LOCATION.kafkaPropertyName()));
-        assertEquals(this.dummyTruststoreFile.toString(),
-                getSecurityEnabledConsumerProperties("SASL_SSL", null, this.dummyTruststoreFile, null).getProperties()
-                        .get(SSL_TRUSTSTORE_LOCATION.kafkaPropertyName()));
-        assertEquals("kpw", getSecurityEnabledConsumerProperties("SASL_SSL", null, null, null).getProperties()
-                .get(SSL_KEY_PASSWORD.kafkaPropertyName()));
     }
 
     @Test
-    void krb5ConfValidation() throws Exception {
+    void applyThrowsIfProvidedKeystoreFileIsNotAvailableWithSslSaslProtocol() {
+        assertMissingSecurityFile("SASL_SSL", Paths.get("ssl_keystore_file"), null, null,
+                "Unable to find the SSL keystore file");
+    }
+
+    @Test
+    void applyThrowsIfProvidedTruststoreFileIsNotAvailableWithSslSaslProtocol() {
+        assertMissingSecurityFile("SASL_SSL", this.dummyKeystoreFile, Paths.get("ssl_truststore_file"), null,
+                "Unable to find the SSL truststore file");
+    }
+
+    @Test
+    void getPropertiesContainsKeystoreFileWithSslSaslProtocol() throws Exception {
+        assertEquals(this.dummyKeystoreFile.toString(),
+                getSecurityEnabledConsumerProperties("SASL_SSL", this.dummyKeystoreFile, null, null).getProperties()
+                        .get(SSL_KEYSTORE_LOCATION.kafkaPropertyName()));
+    }
+
+    @Test
+    void getPropertiesContainsTruststoreFileWithSslSaslProtocol() throws Exception {
+        assertEquals(this.dummyTruststoreFile.toString(),
+                getSecurityEnabledConsumerProperties("SASL_SSL", null, this.dummyTruststoreFile, null).getProperties()
+                        .get(SSL_TRUSTSTORE_LOCATION.kafkaPropertyName()));
+    }
+
+    @Test
+    void getPropertiesContainsKeystoreAndTruststoreFilesWithSslSaslProtocol() throws Exception {
+        final Map<String, Object> props = getSecurityEnabledConsumerProperties("SASL_SSL", this.dummyKeystoreFile,
+                this.dummyTruststoreFile, null).getProperties();
+        assertEquals(this.dummyKeystoreFile.toString(), props.get(SSL_KEYSTORE_LOCATION.kafkaPropertyName()));
+        assertEquals(this.dummyTruststoreFile.toString(), props.get(SSL_TRUSTSTORE_LOCATION.kafkaPropertyName()));
+    }
+
+    @Test
+    void getPropertiesContainsSslRelatedInformationWithSslSaslProtocol() throws Exception {
+        final Map<String, Object> props = getSecurityEnabledConsumerProperties("SASL_SSL", null, null, null)
+                .getProperties();
+        assertEquals("kpw", getSecurityEnabledConsumerProperties("SASL_SSL", null, null, null).getProperties()
+                .get(SSL_KEY_PASSWORD.kafkaPropertyName()));
+        assertEquals("kspw", props.get(SSL_KEYSTORE_PASSWORD.kafkaPropertyName()));
+        assertEquals("tspw", props.get(SSL_TRUSTSTORE_PASSWORD.kafkaPropertyName()));
+    }
+
+    @Test
+    void errorIsThrownWhenNonExistentKrb5ConfFilePassed() throws Exception {
         final var properties = getSecurityEnabledConsumerProperties("SASL_SSL", null, null, Paths.get("krb5_non_existing"));
 
         final KafkaConnectorException thrown = assertThrows(KafkaConnectorException.class, () -> properties.getProperties());
@@ -313,7 +546,12 @@ class KafkaConsumerPropertiesTest {
         assertAll(() -> assertTrue(thrown.getMessage().contains("Unable to find the custom krb5.conf file")),
                 () -> assertTrue(thrown.getMessage().contains("Please make sure it is successfully uploaded to BucketFS bucket")));
 
-        final var props = getSecurityEnabledConsumerProperties("SASL_SSL", null, null, this.dummyKrb5ConfFile).getProperties();
+    }
+
+    @Test
+    void propertyIsSetWhenExistingKrb5ConfFilePassed() throws Exception {
+        final Map<String, Object> props = getSecurityEnabledConsumerProperties("SASL_SSL", null, null,
+                this.dummyKrb5ConfFile).getProperties();
         assertEquals(this.dummyKrb5ConfFile.toString(), props.get(SASL_KRB5CONF_LOCATION.kafkaPropertyName()));
     }
 
